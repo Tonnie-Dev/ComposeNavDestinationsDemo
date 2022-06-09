b758cf135157539add2a94acc3741a678c80f819
Tonnie <vontonnie@gmail.com>
2022-06-09 13:55:23 +0300

    Replace individual argument with an enclosing parcelable object

--- app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
+++ app/src/main/java/com/plcoding/composenavdestinationsdemo/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.plcoding.composenavdestinationsdemo
 
 import android.os.Bundle
+import android.os.Parcelable
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
@@ -8,26 +9,17 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
-import androidx.navigation.NavType
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
 import com.plcoding.composenavdestinationsdemo.ui.theme.ComposeNavDestinationsDemoTheme
 import com.ramcosta.composedestinations.annotation.Destination
-import java.time.Instant
+import kotlinx.parcelize.Parcelize
 import java.time.LocalDateTime
-import java.time.ZoneId
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,83 +34,84 @@ class MainActivity : ComponentActivity() {
                 val navController = rememberNavController()
 
 
-             /*   //NAV HOST
-                NavHost(
-                    navController = navController,
-                    startDestination = "login"
-                ) {
+                /*   //NAV HOST
+                   NavHost(
+                       navController = navController,
+                       startDestination = "login"
+                   ) {
 
-                    //1ST SCREEN COMPOSABLE
-                    composable("login") {
-
-
-                        LoginScreen(navController)
-                    }
+                       //1ST SCREEN COMPOSABLE
+                       composable("login") {
 
 
-                    //2ND SCREEN COMPOSABLE
-                    composable(
-                        route = "profile/{name}/{userId}/{timestamp}",
-
-                            //argument 1
-                        arguments = listOf(
-                            navArgument("name") {
-                                type = NavType.StringType
-                            },
-
-                                //argument 2
-                            navArgument("userId") {
-                                type = NavType.StringType
-                            },
+                           LoginScreen(navController)
+                       }
 
 
-                                //argument 3
-                            navArgument("timestamp") {
-                                type = NavType.LongType
-                            },
-                        )
-                    ) {
+                       //2ND SCREEN COMPOSABLE
+                       composable(
+                           route = "profile/{name}/{userId}/{timestamp}",
 
-                        //Extracting NavArguments from NavBackStackEntry
-                        val name = it.arguments?.getString("name")!!
-                        val userId = it.arguments?.getString("userId")!!
-                        val timestamp = it.arguments?.getLong("timestamp")!!
+                               //argument 1
+                           arguments = listOf(
+                               navArgument("name") {
+                                   type = NavType.StringType
+                               },
 
-                        ProfileScreen(
-                            navController = navController,
-                            name = name,
-                            userId = userId,
-                            created = timestamp
-                        )
-                    }
+                                   //argument 2
+                               navArgument("userId") {
+                                   type = NavType.StringType
+                               },
 
-                    //3RD SCREEN COMPOSABLE
-                    composable("post/{showOnlyPostsByUser}", arguments = listOf(
 
-                            //argument 1
-                        navArgument("showOnlyPostsByUser") {
-                            type = NavType.BoolType
-                            defaultValue = false
-                        }
-                    )) {
-                        val showOnlyPostsByUser =
-                            it.arguments?.getBoolean("showOnlyPostsByUser") ?: false
-                        PostScreen(showOnlyPostsByUser)
-                    }
-                }*/
+                                   //argument 3
+                               navArgument("timestamp") {
+                                   type = NavType.LongType
+                               },
+                           )
+                       ) {
+
+                           //Extracting NavArguments from NavBackStackEntry
+                           val name = it.arguments?.getString("name")!!
+                           val userId = it.arguments?.getString("userId")!!
+                           val timestamp = it.arguments?.getLong("timestamp")!!
+
+                           ProfileScreen(
+                               navController = navController,
+                               name = name,
+                               userId = userId,
+                               created = timestamp
+                           )
+                       }
+
+                       //3RD SCREEN COMPOSABLE
+                       composable("post/{showOnlyPostsByUser}", arguments = listOf(
+
+                               //argument 1
+                           navArgument("showOnlyPostsByUser") {
+                               type = NavType.BoolType
+                               defaultValue = false
+                           }
+                       )) {
+                           val showOnlyPostsByUser =
+                               it.arguments?.getBoolean("showOnlyPostsByUser") ?: false
+                           PostScreen(showOnlyPostsByUser)
+                       }
+                   }*/
             }
         }
     }
 }
-@Destination
+
+@Destination(start = true)
 @Composable
 fun LoginScreen(
     navController: NavController
 ) {
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Login Screen")
         Button(onClick = {
@@ -134,11 +127,9 @@ fun LoginScreen(
 @Composable
 fun ProfileScreen(
     navController: NavController,
-    name: String,
-    userId: String,
-    created: Long
+    user: User
 ) {
-    val user = remember {
+    /*val user = remember {
         User(
             name = name,
             id = userId,
@@ -146,11 +137,11 @@ fun ProfileScreen(
                 Instant.ofEpochMilli(created), ZoneId.systemDefault()
             )
         )
-    }
+    }*/
     Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Profile Screen: $user", textAlign = TextAlign.Center)
         Button(onClick = {
@@ -161,14 +152,22 @@ fun ProfileScreen(
     }
 }
 
+@Parcelize
+data class UserClass(
+    val name: String,
+    val id: String,
+    val created: LocalDateTime
+) : Parcelable
+
+
 @Destination
 @Composable
 fun PostScreen(
     showOnlyPostsByUser: Boolean = false
 ) {
     Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
     ) {
         Text(text = "Post Screen, $showOnlyPostsByUser")
     }
